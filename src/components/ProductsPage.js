09e44ff78a3231f311232fa13c5421028be659b6
cleverjack <example@test.com>
2020-03-07 10:17:57 -0400

    first commit

---
+++ src/components/ProductsPage.js
@@ -0,0 +1,13 @@
+import React from 'React';
+
+
+export default class ProductsPage extends React.Component{
+
+render(){
+    return (
+        <div style={{padding:"20px"}}>
+            this is from products page
+            </div>
+    );
+}
+}
---
+++ src/components/TestComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ServicesPage = () => (
+  <div style={{margin:20}}>
+    this is test component! OK!
+  </div>
+);
+
+export default ServicesPage;
